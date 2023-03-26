3cc0a0f433a5048dc09650a899ff5e8e95d03ac8
oleg <oleg20064@gmail.com>
2023-03-26 15:44:56 +0200

    Правки кода для оптимизации

--- handler.py
+++ handler.py
@@ -1,10 +1,13 @@
+import concurrent
+import io
+from concurrent.futures import ThreadPoolExecutor
 from typing import List
 
 import aiofiles
 import asyncio
 import csv
 from bs4 import BeautifulSoup
-from aiohttp import ClientSession, ClientTimeout
+from aiohttp import ClientSession, ClientTimeout, TCPConnector
 from urllib.parse import urlparse, urljoin
 
 from fake_headers import Headers
@@ -21,11 +24,9 @@ class Handler:
         self.timeout = ClientTimeout(total=10)
 
     async def main(self):
-        tasks = []
-        for link in self.start_urls:
-            task = asyncio.create_task(self.search_recursive(link, depth=0))
-            tasks.append(task)
-        await asyncio.gather(*tasks)
+        async with ClientSession(headers=self.headers, connector=TCPConnector(limit=10)) as session:
+            tasks = [asyncio.create_task(self.search_recursive(session, link, depth=0)) for link in self.start_urls]
+            await asyncio.gather(*tasks)
 
     async def write_to_csv(self, data_list):
         filename = "output.csv"
@@ -35,7 +36,7 @@ class Handler:
                 writer.writeheader()
             await asyncio.gather(*[writer.writerow(data) for data in data_list])
 
-    async def search_recursive(self, link, depth):
+    async def search_recursive(self, session, link, depth):
         parsed_link = urlparse(link)
         await asyncio.sleep(1)
         if f"{parsed_link.scheme}://{parsed_link.hostname}" in self.visited_domains:
@@ -45,15 +46,14 @@ class Handler:
         if depth > 2:
             return
         self.visited_urls.add(link)
-        async with ClientSession(headers=self.headers) as session:
-            async with session.get(link) as response:
-                if response.status != 200:
-                    return
-                try:
-                    soup = BeautifulSoup(await response.text(), "lxml")
-                except UnicodeDecodeError as err:
-                    print(f"С сайта {link} пришла неверная кодировка - {err}")
-                    return
+        async with session.get(link) as response:
+            if response.status != 200:
+                return
+            try:
+                soup = BeautifulSoup(await response.text(), "lxml")
+            except UnicodeDecodeError as err:
+                print(f"С сайта {link} пришла неверная кодировка - {err}")
+                return
         if soup.find(self.tag):
             if f"{parsed_link.scheme}://{parsed_link.hostname}" in self.visited_domains:
                 return
@@ -67,13 +67,14 @@ class Handler:
         parsed_url = urlparse(link)
         base_url = f"{parsed_url.scheme}://{parsed_url.hostname}"
         tasks = []
-        for a in soup.find_all('a', href=True):
-            href = a['href']
-            if href.startswith('/'):
-                href = urljoin(base_url, href)
-            if not href.startswith(base_url):
-                continue
-            task = asyncio.create_task(self.search_recursive(href, depth=depth + 1))
-            tasks.append(task)
-        await asyncio.sleep(1)
-        return await asyncio.gather(*tasks)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+            for a in soup.find_all('a', href=True):
+                href = a['href']
+                if href.startswith('/'):
+                    href = urljoin(base_url, href)
+                if not href.startswith(base_url):
+                    continue
+                task = asyncio.ensure_future(self.search_recursive(session, href, depth=depth + 1))
+                tasks.append(task)
+        await asyncio.sleep(2)
+        await asyncio.gather(*tasks)
