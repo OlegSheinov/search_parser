3b9255fe084a6a0b019610cb09d04c18eddbcad9
oleg <oleg20064@gmail.com>
2023-03-26 16:28:14 +0200

    Последние правки кода

--- handler.py
+++ handler.py
@@ -1,80 +1,74 @@
+import asyncio
 import concurrent
-import io
+import csv
 from concurrent.futures import ThreadPoolExecutor
 from typing import List
-
-import aiofiles
-import asyncio
-import csv
-from bs4 import BeautifulSoup
-from aiohttp import ClientSession, ClientTimeout, TCPConnector
 from urllib.parse import urlparse, urljoin
 
+import aiofiles
+from aiocsv import AsyncDictWriter
+from aiohttp import ClientSession, ClientTimeout, TCPConnector, ClientError
+from bs4 import BeautifulSoup
 from fake_headers import Headers
 
 
 class Handler:
-    def __init__(self, data: List[str], tag: str):
+    def __init__(self, data: List[str], tag: str, query: str):
+        self.query = query
         self.start_urls = data
         self.tag = tag
         self.visited_urls = set()
         self.visited_domains = set()
         self.headers = Headers(headers=True).generate()
-        self.fieldnames = ["current_page", "sub_page"]
+        self.fieldnames = ["current_page", "sub_page", "query"]
         self.timeout = ClientTimeout(total=10)
 
     async def main(self):
-        async with ClientSession(headers=self.headers, connector=TCPConnector(limit=10)) as session:
+        async with ClientSession(headers=self.headers, connector=TCPConnector(limit=10),
+                                 timeout=self.timeout) as session:
             tasks = [asyncio.create_task(self.search_recursive(session, link, depth=0)) for link in self.start_urls]
-            await asyncio.gather(*tasks)
+            await asyncio.gather(*tasks, return_exceptions=True)
 
     async def write_to_csv(self, data_list):
-        filename = "output.csv"
+        filename = f"output_{self.tag}.csv"
         async with aiofiles.open(filename, mode="a", encoding="utf-8", newline="") as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames, delimiter=";")
+            writer = AsyncDictWriter(csvfile, fieldnames=self.fieldnames, delimiter=";")
             if not csvfile.tell():
-                writer.writeheader()
-            await asyncio.gather(*[writer.writerow(data) for data in data_list])
+                await writer.writeheader()
+            await asyncio.gather(*[await writer.writerow(data) for data in data_list])
 
-    async def search_recursive(self, session, link, depth):
+    async def search_recursive(self, session: ClientSession, link, depth):
         parsed_link = urlparse(link)
-        await asyncio.sleep(1)
-        if f"{parsed_link.scheme}://{parsed_link.hostname}" in self.visited_domains:
-            return
-        if link in self.visited_urls:
-            return
-        if depth > 2:
+        if link in self.visited_urls or depth > 2:
             return
         self.visited_urls.add(link)
-        async with session.get(link) as response:
-            if response.status != 200:
-                return
-            try:
+        try:
+            async with session.get(link) as response:
+                if response.status != 200:
+                    return
                 soup = BeautifulSoup(await response.text(), "lxml")
-            except UnicodeDecodeError as err:
-                print(f"С сайта {link} пришла неверная кодировка - {err}")
-                return
+        except (ClientError, TimeoutError, UnicodeDecodeError):
+            return
         if soup.find(self.tag):
             if f"{parsed_link.scheme}://{parsed_link.hostname}" in self.visited_domains:
                 return
             parsed_url = urlparse(link)
             sub_url = f"{parsed_url.scheme}://{parsed_url.hostname}{parsed_url.path}"
-            print(f'Найдено на {sub_url}')
             self.visited_domains.add(f"{parsed_url.scheme}://{parsed_url.hostname}")
-            await asyncio.sleep(1)
+            print(f'Найдено на {sub_url}')
             return await self.write_to_csv(
-                [{self.fieldnames[0]: f"{parsed_url.scheme}://{parsed_url.hostname}", self.fieldnames[1]: sub_url}])
+                [{self.fieldnames[0]: f"{parsed_url.scheme}://{parsed_url.hostname}", self.fieldnames[1]: sub_url,
+                  self.fieldnames[2]: self.query}])
         parsed_url = urlparse(link)
         base_url = f"{parsed_url.scheme}://{parsed_url.hostname}"
         tasks = []
-        with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=20):
             for a in soup.find_all('a', href=True):
                 href = a['href']
                 if href.startswith('/'):
                     href = urljoin(base_url, href)
                 if not href.startswith(base_url):
                     continue
-                task = asyncio.ensure_future(self.search_recursive(session, href, depth=depth + 1))
+                task = asyncio.create_task(self.search_recursive(session, href, depth=depth + 1))
                 tasks.append(task)
-        await asyncio.sleep(2)
-        await asyncio.gather(*tasks)
+            await asyncio.gather(*tasks)
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import asyncio
-import sys
-
 from urllib.parse import urlparse
 
 import aiofiles
@@ -21,37 +19,35 @@ class Parser:
 
     async def parsing(self):
         async with ClientSession() as session:
-            for page in range(1, 3):
-                # url = f"https://customsearch.googleapis.com/customsearch/v1?cx={os.getenv('CX_KEY')}&lr=lang_en" \
-                #       f"&q={quote(query)}{f'&start={10 * (page - 1)}' if page > 1 else ''}&num200&key={os.getenv('API_KEY')}"
+            for page in range(1, 21):
                 url = f"https://www.google.com/search?q={self.query}{f'&start={10 * (page - 1)}' if page > 1 else ''}"
-                # url = f"https://cse.google.com/cse/element/v1?num=200&hl=ru&cx={os.getenv('CX_KEY')}&q={query}" \
-                #       f"&cse_tok=AFW0emxq9B2hAHXgkISR4NDoJ_UI:1679764553222&callback=google.search.cse.api18571"
-                # task = asyncio.ensure_future(parse_url(url))
-                # tasks.append(task)
                 headers = Headers(
                     headers=True
                 ).generate()
                 try:
+                    # TODO раскомментировать этот код, если нужно использование прокси
+                    # proxy = "https://196.18.224.210:8000"
+                    # proxy_auth = BasicAuth("S5Z5dU", "Xr0qPk")
+                    # async with session.get(url, headers=headers, proxy=proxy, proxy_auth=proxy_auth) as response:
                     async with session.get(url, headers=headers) as response:
+                        if response.status != 200:
+                            raise ConnectionError(
+                                "Слишком много запросов. Повторите попытку через 10 минут или смените IP-адрес")
                         data = await response.read()
                     soup = BeautifulSoup(data, "lxml")
                     all_links = [item.find("a").get("href") for item in
                                  soup.find_all("div", class_="yuRUbf")]
                     self.all_links_from_google.extend(
                         [urlparse(item).scheme + f"://{urlparse(item).netloc}" for item in all_links])
-                except BaseException as err:
+                except TimeoutError as err:
                     print(err)
                     await session.close()
         self.all_links_from_google = set(self.all_links_from_google)
         await self.parse_url()
 
     async def parse_url(self):
-        try:
-            handler = Handler(list(self.all_links_from_google), "figure")
-            print(await handler.main())
-        except KeyError as err:
-            print(err)
+        handler = Handler(list(self.all_links_from_google), "figure", self.query)
+        await handler.main()
 
 
 async def start():
